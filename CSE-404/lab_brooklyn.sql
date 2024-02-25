c80ee4651a5398addec86cc45856f05c8c640481
Tashrif <117228564+Tashrif2001@users.noreply.github.com>
2024-02-25 15:47:38 +0600

    Create lab_brooklyn.sql

---
+++ CSE-404/lab_brooklyn.sql
@@ -0,0 +1,1 @@
+select distinct customer_name from depositor D, account A where D.account_number = A.account_number and (branch_name) in (select branch_name from branch where branch_city='BROOKLYN') group by customer_name having count(*)= (select count(branch_name) from branch where branch_city='BROOKLYN');
