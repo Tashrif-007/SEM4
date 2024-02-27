8602a6c02e339b36c9084a62579db4775477099d
Tashrif <117228564+Tashrif2001@users.noreply.github.com>
2024-02-27 23:23:44 +0600

    Update fcfs.cpp

--- CSE-401/fcfs.cpp
+++ CSE-401/fcfs.cpp
@@ -7,6 +7,9 @@ struct process {
     int arrival;
     int start;
     int end;
+    int index;
+    int turn;
+    int wait;
 };
 
 int main() {
@@ -16,16 +19,21 @@ int main() {
     process processes[num];
 
     cout << "Enter burst time and priority for each process\n";
-    for(auto &x : processes) {
-        cin >> x.burst_time >> x.priority;
-        x.arrival = 0;
-        x.start = 0;
-        x.end = x.burst_time;
+    for(int i=0; i<num; i++) {
+        cin >> processes[i].burst_time >> processes[i].priority;
+        processes[i].arrival = 0;
+        processes[i].start = 0;
+        processes[i].end = processes[i].burst_time;
+        processes[i].index = i+1;
+        processes[i].wait=0;
+        processes[i].turn = processes[i].burst_time;
     }
     
     for(int i = 1; i < num; i++) {
         processes[i].start = processes[i - 1].burst_time + processes[i - 1].start;
         processes[i].end = processes[i].start + processes[i].burst_time;
+        processes[i].turn = processes[i].end - processes[i].arrival;
+        processes[i].wait = processes[i-1].wait + processes[i-1].burst_time;
     }
 
     cout<<"Gantt Chart\n";
@@ -46,5 +54,33 @@ int main() {
     cout << "|";
     cout << endl;
 
+    for(int i=0; i<num; i++)
+    {
+        for(int j=processes[i].start; j<processes[i].end; j++)
+        {
+            if(j==processes[i].start)
+                cout<<j;
+            else
+            cout<<" ";
+        }
+    }
+    cout<<processes[num-1].end<<endl;
+
+    cout<<"Turnaround and Waiting times: \n";
+    for(auto x : processes)
+    {
+        cout<<"P"<<x.index<<" Turn: "<<x.turn<<" Wait: "<<x.wait<<endl;
+    }
+    int sum=0;
+    cout<<"Average Turn Around Time: \n";
+    for(auto x : processes)
+    sum+=x.turn;
+    cout<<(double)sum/num<<endl;
+
+    sum=0;
+    cout<<"Average Waiting Time: \n";
+    for(auto x : processes)
+    sum+=x.wait;
+    cout<<(double)sum/num<<endl;
     return 0;
 }
