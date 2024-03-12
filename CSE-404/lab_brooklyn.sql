efb21b88c7043095d38ec61a2de2eb83afe14cf8
Tashrif <117228564+Tashrif2001@users.noreply.github.com>
2024-03-12 23:32:44 +0600

    Update lab_brooklyn.sql

--- CSE-404/lab_brooklyn.sql
+++ CSE-404/lab_brooklyn.sql
@@ -1,1 +1,7 @@
-select distinct customer_name from depositor D, account A where D.account_number = A.account_number and (branch_name) in (select branch_name from branch where branch_city='BROOKLYN') group by customer_name having count(*)= (select count(branch_name) from branch where branch_city='BROOKLYN');
+select distinct customer_name 
+from depositor D, account A 
+where D.account_number = A.account_number 
+and (branch_name) in 
+(select branch_name from branch where branch_city='BROOKLYN')
+group by customer_name 
+having count(*)= (select count(branch_name) from branch where branch_city='BROOKLYN');
