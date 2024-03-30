0cfa2df1204004c72173374d3ebc9836adeb8677
Tashrif <117228564+Tashrif-007@users.noreply.github.com>
2024-03-30 22:10:51 +0600

    padding done

--- CSE-411/sha_512.cpp
+++ CSE-411/sha_512.cpp
@@ -19,13 +19,18 @@ void padding(string &plain)
     {
         plain+='\x00';
     }
+    int dig = log10((uint64_t)size)+1;
+    int rem = 32-dig;
+    while(rem--)
+    plain+='\x00';
+    plain+=(uint8_t)size;
 }
 int main()
 {
     string plain;
     cin>>plain;
     padding(plain);
-    for(int i=0; i<plain.size(); i++)
+    for(int i=0; i<252; i++)
     printf("%x", plain[i]);
     return 0;
 }
